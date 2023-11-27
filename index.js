4bd98382d7d4f29408ff5de638d22d971ee5586b
Anselmo Del Hoyo <anselmodelhoyo11@gmail.com>
2023-11-27 23:41:35 +0100

    Values in JavaScript

---
+++ index.js
@@ -0,0 +1,14 @@
+// ====== Capítulo 1: Valores, Tipos y Operadores
+
+// ====== Valores
+
+/*
+    Imagina un mar de bits, un océano de ellos. Un computadora moderna
+    promedio tiene más de 30 billones de bits en su almacenamiento de datos volátiles
+    (memoria funcional). El almacenamiento no volátil (disco duro o equivalente)
+    tiende a tener unas cuantas más ordenes de magnitud.
+
+    Para poder trabajar con tales cantidades de bits sin perdernos, debemos separarlos
+    en porciones que representen pedazos de información. En un entorno de JavaScript, esas
+    porciones son llamadas valores.
+*/
