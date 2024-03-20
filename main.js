12110c71e87b957dec3d2d5fe9f792af241f2d52
Anselmo Del Hoyo <anselmodelhoyo11@gmail.com>
2024-03-20 19:10:29 +0100

    Destructuring arrays and objects

--- main.js
+++ main.js
@@ -2052,3 +2052,45 @@ console.log(Math.floor(Math.random() * 10));
     número, lo que significa que niega los valores negativos pero deja los positivos tal y como estan.
 */
 
+// ====== Desestructurar
+
+/*
+    La función phi: 
+*/
+
+function phi(tabla) {
+    return (tabla[3] * tabla[0] - tabla[2] * tabla[1]) /
+    Math.sqrt((tabla[2] + tabla[3]) *
+    (tabla[0] + tabla[1]) *
+    (tabla[1] + tabla[3]) *
+    (tabla[0] + tabla[2]));
+}
+
+/*
+    Una de las razones por las que esta función es incómoda de leer es que
+    tenemos una vinculación apuntando a nuestro array, pero preferiríamos tener
+    vinculaciones para los elementos del array, es decir, let n00 = tabla[0] y así
+    sucesivamente. Afortunadamente, hay una forma concisa de hacer esto en
+    JavaScript.
+*/
+
+function phi([n00, n01, n10, n11]) {
+    return (n11 * n00 - n10 * n01) /
+    Math.sqrt(
+        (n10 + n11) * (n00 + n01) *
+        (n01 + n11) * (n00 + n10)
+    );
+}
+
+// Esto también funciona para vinculaciones creadas con let, var, o const. Si
+// sabes que el valor que estas vinculando es un array, puedes usar corchetes para
+// “mirar dentro” del valor, y asi vincular sus contenidos.
+// Un truco similar funciona para objetos, utilizando llaves en lugar de corchetes.
+
+let {nombre} = {nombre: "Faraji", edad: 23};
+console.log(nombre);
+// -> Faraji
+
+// Ten en cuenta que si intentas desestructurar null o undefined, obtendrás un
+// error, igual como te pasaria si intentaras acceder directamente a una propiedad
+// de esos valores.
