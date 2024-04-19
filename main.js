e3760cddcc93cf72565bed1ca9315a08794a56a1
Anselmo Del Hoyo <anselmodelhoyo11@gmail.com>
2024-04-19 18:10:56 +0200

    High order functions

--- main.js
+++ main.js
@@ -2296,4 +2296,61 @@ let etiquetas = [];
 repetir(5, i => {
     etiquetas.push(`Unidad ${i + 1}`);
 });
-console.log(etiquetas)
+console.log(etiquetas); 
+
+// ====== Funciones de Orden Superior
+
+/*
+    Las funciones que operan en otras funciones, ya sea tomándolas como argumen-
+    tos o retornandolas, se denominan "funciones de Orden Superior". El término
+    proviene de las matemáticas, donde la distinción entre funciones y otros va-
+    lores se toma más enserio.
+
+    Las funcioenes de orden superior nos permiten abstraer sobre acciones, no solo
+    sobre valores. Estas vienen en varias formas. Por ejemplo, puedes tener funciones
+    que crean nuevas funciones.
+*/
+
+function mayorQue(n) {
+    return m => m > n;
+}
+
+let mayorQue10 = mayorQue(10);
+console.log(mayorQue10(11));
+
+// Y puedes tener funciones que cambien otras funciones.
+
+function ruidosa(funcion) {
+    return (...argumentos) => {
+        console.log("llamando con", argumentos);
+        let resultado = funcion(...argumentos);
+        console.log("llamada con", argumentos, ", retorno", resultado);
+        return resultado;
+    }
+}
+ruidosa(Math.min)(3,2,1);
+// -> llamando con [3, 2, 1]
+
+/*
+    Incluso se pueden escribir funciones que proporcionen nuevos tipos
+    de flujo de control.
+*/
+
+function aMenosQue(prueba, entonces) {
+    if (!prueba) entonces();
+}
+
+repetir(3, n => {
+    aMenosQue(n % 2 == 1, () =>  {
+        console.log(n, "es par");
+    });
+});
+
+/*
+    Hay un método de array incorporado, forEach que proporciona algo como
+    un ciclo for/of como una función de orden superior.
+*/
+
+["A", "B"].forEach(letra => console.log(letra));
+// -> A
+// -> B
