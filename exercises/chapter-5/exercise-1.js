9e291b1695969d86b7ff19b42d587dd6d463fbec
Anselmo Del Hoyo <anselmodelhoyo11@gmail.com>
2024-05-17 17:59:58 +0200

    Flattening arrays

---
+++ exercises/chapter-5/exercise-1.js
@@ -0,0 +1,18 @@
+// Aplanamiento
+
+/*
+    Use el método de reduce en combinación con el método concat para "aplanar" un
+    array de arrays en un único array que tenga todos los elementos de los arrays
+    originales.
+*/
+
+let arrays = [1, 2, 3, [4, 5, 6]];
+
+function aplanarArray(array = []) {
+
+    let arrayFinal = array.reduce((previous, current) => previous.concat(current), [])
+
+    return arrayFinal;
+}
+
+console.log(aplanarArray(arrays));
